d4ff4e488cdbb46c9f269658f0934c895a63df8e
Daniel Gonzalez <danielgnzch@gmail.com>
2022-05-27 20:22:58 +0200

    lab done

--- app.js
+++ app.js
@@ -1,20 +1,31 @@
 // Require Express
-const express = require('express');
+const express = require("express");
 
 // Express server handling requests and responses
 const app = express();
 
 // Make everything inside of public/ available
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // Root path access redirects to static home.html file.
-app.get('/', (request, response, next) => response.sendFile(__dirname + '/views/home.html'));
+app.get("/", (request, response, next) =>
+  response.sendFile(__dirname + "/views/home.html")
+);
 
 // about route:
-app.get('/about', (request, response, next) => response.sendFile(__dirname + '/views/about.html'));
+app.get("/about", (request, response, next) =>
+  response.sendFile(__dirname + "/views/about.html")
+);
 
-// songs route:
-app.get('/songs', (request, response, next) => response.sendFile(__dirname + '/views/songs.html'));
+// works route:
+app.get("/works", (request, response, next) =>
+  response.sendFile(__dirname + "/views/works.html")
+);
+
+// photogallery route:
+app.get("/photogallery", (request, response, next) =>
+  response.sendFile(__dirname + "/views/photogallery.html")
+);
 
 // Server Started
-app.listen(3000, () => console.log('My first app listening on port 3000!'));
+app.listen(3000, () => console.log("My first app listening on port 3000!"));
